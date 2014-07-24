beec368b285290e63771d57f23f592d4e8fc8cbe
Eugene Zimichev <undefz@gmail.com>
2014-07-24 16:32:20 +0400

    Added support for test directories

--- src/ru/undefz/IvyPropertiesProvider.java
+++ src/ru/undefz/IvyPropertiesProvider.java
@@ -1,8 +1,10 @@
 package ru.undefz;
 
+import com.google.common.base.Optional;
 import com.intellij.ide.fileTemplates.DefaultTemplatePropertiesProvider;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
+import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -17,15 +19,20 @@ import java.util.Properties;
  * @since 1.0
  */
 public class IvyPropertiesProvider implements DefaultTemplatePropertiesProvider {
+    private static final Logger log = Logger.getLogger(IvyPropertiesProvider.class);
+
 
     public static final String DEFAULT_VALUE = "";
 
     @Override
     public void fillProperties(PsiDirectory psiDirectory, Properties properties) {
-        properties.put("IVY_COMPONENT_VERSION", extractIvyVersion(psiDirectory));
+        log.info("Starting ivy scanning process");
+        final String propertyValue = extractIvyVersion(psiDirectory);
+        log.info("Founded value " + propertyValue);
+        properties.put("IVY_COMPONENT_VERSION", propertyValue);
     }
 
-    private Object extractIvyVersion(PsiDirectory psiDirectory) {
+    private String extractIvyVersion(PsiDirectory psiDirectory) {
         final String ivyText = searchIvyConfig(psiDirectory);
         if (ivyText != null)
         {
@@ -37,11 +44,12 @@ public class IvyPropertiesProvider implements DefaultTemplatePropertiesProvider 
     }
 
     private String extractDeliverableVersion(String ivyText) {
+        log.info("Parsing ivy file");
         try {
             final Document ivyDocument = new SAXBuilder().build(new StringReader(ivyText));
             final Element childElement = ivyDocument.getRootElement().getChild("info");
             if (childElement != null)
-                childElement.getAttributeValue("revision");
+                return childElement.getAttributeValue("revision");
         } catch (JDOMException | IOException e) {
             throw new RuntimeException(e);
         }
@@ -51,9 +59,26 @@ public class IvyPropertiesProvider implements DefaultTemplatePropertiesProvider 
     private String searchIvyConfig(PsiDirectory psiDirectory) {
         if (psiDirectory == null)
             return null;
+        log.info("Scanning directory " + psiDirectory.getName());
         final PsiFile ivyFile = psiDirectory.findFile("ivy.xml");
         if (ivyFile != null)
+        {
+            log.info("Found ivy file " + ivyFile.getName());
             return ivyFile.getText();
+        }
+
+        if ("test".equals(psiDirectory.getName()))
+        {
+            final PsiDirectory parent = psiDirectory.getParentDirectory();
+            if (parent != null)
+            {
+                final PsiDirectory sourceDirectory = parent.findSubdirectory("source");
+                if (sourceDirectory != null)
+                {
+                    return searchIvyConfig(sourceDirectory);
+                }
+            }
+        }
         return searchIvyConfig(psiDirectory.getParentDirectory());
     }
 }
